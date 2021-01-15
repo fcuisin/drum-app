993b2cc3d0021b00dcfb74888012a31c165cab94
Ferdinand Cuisin <fcuisin@gmail.com>
2021-01-15 15:26:45 +0100

    Create logic betweenk sounds and keyboard

--- js/main.js
+++ js/main.js
@@ -4,9 +4,17 @@ function removeTranisition(key) {
   });
 }
 
+function playMusic(audio, key) {
+  key.classList.add('play');
+  audio.currentTime = 0;
+  audio.play();
+}
+
 window.addEventListener("keydown", function(event) {
   const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
-  key.classList.add('play');
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  if (!key) return;
+  playMusic(audio, key);
   removeTranisition(key);
 });
 
